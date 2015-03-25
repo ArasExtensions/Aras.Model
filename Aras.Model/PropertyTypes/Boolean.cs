e452cb451825c7f7c622a55f89c104afa20ceb98
Richard Mason <richard.mason@processwall.com>
2015-03-25 18:04:43 +0000

    Updated link in header to osi website

--- Aras.Model/PropertyTypes/Boolean.cs
+++ Aras.Model/PropertyTypes/Boolean.cs
@@ -15,7 +15,7 @@
   GNU Affero General Public License for more details.
 
   You should have received a copy of the GNU Affero General Public License
-  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+  along with this program.  If not, see http://opensource.org/licenses/AGPL-3.0.
  
   Company: Processwall Limited
   Address: The Winnowing House, Mill Lane, Askham Richard, York, YO23 3NW, United Kingdom
