89c9d6a7d819d98cb398b5f12b2477731688569a
Richard Mason <richard.mason@aessis.com>
2015-06-15 16:35:47 +0000

    Updated login to require MD5 hash password

--- Aras.Model/Database.cs
+++ Aras.Model/Database.cs
@@ -38,17 +38,16 @@ namespace Aras.Model
 
         public String Name { get; private set; }
 
-        public Session Login(String Username, String Password)
+        public static String PasswordHash(String Password)
         {
-            // Get MD5 Hash of Password
             String md5password = null;
 
-            using(MD5 md5 = MD5.Create())
+            using (MD5 md5 = MD5.Create())
             {
                 byte[] data = md5.ComputeHash(Encoding.UTF8.GetBytes(Password));
                 StringBuilder md5string = new StringBuilder();
 
-                for(int i=0; i<data.Length; i++)
+                for (int i = 0; i < data.Length; i++)
                 {
                     md5string.Append(data[i].ToString("x2"));
                 }
@@ -56,13 +55,18 @@ namespace Aras.Model
                 md5password = md5string.ToString();
             }
 
-            IO.SOAPRequest request = new IO.SOAPRequest(IO.SOAPOperation.ValidateUser, this, Username, md5password);
+            return md5password;
+        }
+
+        public Session Login(String Username, String Password)
+        {
+            IO.SOAPRequest request = new IO.SOAPRequest(IO.SOAPOperation.ValidateUser, this, Username, Password);
             IO.SOAPResponse response = request.Execute();
 
             if (!response.IsError)
             {
                 String id = response.Result.SelectSingleNode("id").InnerText;
-                Session session = new Session(this, id, Username, md5password);
+                Session session = new Session(this, id, Username, Password);
                 return session;
             }
             else
